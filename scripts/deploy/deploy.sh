31da85a8dd960e43240d6e520fa8054ac3568e9e
angusfz <angusfz@gmail.com>
2019-01-23 09:06:44 +0800

    Move var to travis system

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -3,13 +3,13 @@
 
 # Vaiables
 export ECS_CLUSTER="Cluster-${ECS_SERVICE}"
-export TASK_DEFINITION_TEMPLATE="${TRAVIS_BUILD_DIR}/conf/task-definition.json"
+export TASK_DEFINITION_TEMPLATE="${TRAVIS_BUILD_DIR}/scripts/deploy/task-definition.json"
 export TASK_DEFINITION_FAMILY="${ECS_SERVICE}-TaskDefinition"
 export TASK_DEFINITION_EXECUTION_ROLE_ARN="${ECS_SERVICE}-ExecutionRole"
 export TASK_DEFINITION_TASK_ROLE_ARN="${ECS_SERVICE}-TaskRole"
 export ECR_URI="${AWS_ACCOUNTID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_SERVICE}"
 export ECR_URI_TAG_LATEST=${ECR_URI}:latest
-export ECR_URI_TAG_CUSTOM=${ECR_URI}:${IMAGE_TAG}
+export ECR_URI_TAG_CUSTOM=${ECR_URI}:${TRAVIS_COMMIT}
 export CLW_LOG_GROUP="/ecs/${TASK_DEFINITION_FAMILY}"
 
 if [[ ${TRAVIS_REPO_SLUG} != "104corp/hydra" ]]; then
@@ -22,11 +22,11 @@ if [[ ${AWS_ACCESS_KEY_ID} == "" ]]; then
 fi
 
 # Install AWS cli
-pip install --user awscli
+pip install --user awscli jq
 export PATH=$PATH:$HOME/.local/bin
 
 # Handle sts
-source ${TRAVIS_BUILD_DIR}/scripts/sts.sh
+source ${TRAVIS_BUILD_DIR}/scripts/deploy/sts.sh
 
 # ECR login
 eval $(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION})
@@ -42,7 +42,10 @@ docker push ${ECR_URI_TAG_LATEST}
 docker push ${ECR_URI_TAG_CUSTOM}
 
 # This will replace '$$ENV_VAR_NAME$$' in ${TASK_DEFINITION_TEMPLATE} with envirement variable
-# Using ${TRAVIS_COMMIT} as ${IMAGE_TAG}
+# Example
+# $$ECR_URI_TAG_CUSTOM$$ : Image with commit sha
+# $$CLW_LOG_GROUP$$ : log group
+
 str=`cat ${TASK_DEFINITION_TEMPLATE}`
 while [[ $str =~ ('$$'([[:alnum:]_]+)'$$') ]]; do
     str=${str//${BASH_REMATCH[1]}/${!BASH_REMATCH[2]}}
