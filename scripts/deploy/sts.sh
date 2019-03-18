8dddf754e08e53ef13de74e2b79bc77c26c4acc3
angusfz <angusfz@gmail.com>
2019-03-18 10:25:19 +0800

    修改 環境變數名稱 AWS_ACCOUNTID > AWS_ACCOUNT_ID

--- scripts/deploy/sts.sh
+++ scripts/deploy/sts.sh
@@ -7,4 +7,4 @@ temp_role=$( aws sts assume-role --role-arn arn:aws:iam::$accountid:role/$ROLE_N
 export AWS_ACCESS_KEY_ID=$( echo $temp_role | jq -r .Credentials.AccessKeyId )
 export AWS_SECRET_ACCESS_KEY=$( echo $temp_role | jq -r .Credentials.SecretAccessKey )
 export AWS_SESSION_TOKEN=$( echo $temp_role | jq -r .Credentials.SessionToken )
-export AWS_ACCOUNTID=$accountid
+export AWS_ACCOUNT_ID=$accountid
